19733bf7275ba2c7f957b83ea96085e46e5cef56
yon <Lighthawky@gmail.com>
2012-05-09 18:03:01 +0300

    added cool method for getting values faster

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -9,8 +9,19 @@ class Drink < ActiveRecord::Base
   belongs_to :variation
 
   attr_accessible :name, :note
+  attr_accessible :drink_type_id
 
   def description
     "a coffee"
   end
+
+  [:drink_type, :sugar_type, :sugar_amount, :milk_type, :milk_amount, :strength, :variation].each do |attribute|
+    old_attribute = :"old_#{attribute}"
+    alias_method old_attribute, attribute
+    define_method(attribute) do
+      object = send(old_attribute)
+      ActiveSupport::StringInquirer.new(object.value)
+    end
+  end
+
 end
