a8ae672af08929b37eb43629880c51c338f6e0a8
yon <Lighthawky@gmail.com>
2012-05-10 00:27:04 +0300

    added edit drink and delete drink

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,6 +1,7 @@
 class DrinksController < ApplicationController
 
   before_filter :must_be_logged_in
+  before_filter :load_drink, :only => [:update, :show, :destroy]
 
   def new
     @drink = current_user.drinks.build(:drink_type_id => 1,
@@ -21,9 +22,32 @@ class DrinksController < ApplicationController
     end
   end
 
+  def update
+    @drink.update_attributes(params[:drink])
+    if @drink.save
+      redirect_to drinks_path, :notice => "Drink updated"
+    else
+      render :action => :new
+    end
+  end
+
   def index
     @drinks = current_user.drinks
   end
 
+  def show
+    render :template => "drinks/new"
+  end
+
+  def destroy
+    @drink.update_attribute(:user, nil)
+    redirect_to drinks_path, :notice => "Drink deleted"
+  end
+
+  private
+
+  def load_drink
+    @drink ||= current_user.drinks.find(params[:id])
+  end
 
 end
