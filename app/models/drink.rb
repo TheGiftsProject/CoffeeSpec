932d587f4352131bbcbc98feedde84854df58a36
yon <Lighthawky@gmail.com>
2012-05-09 19:24:30 +0300

    basic application layout

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -12,7 +12,38 @@ class Drink < ActiveRecord::Base
   attr_accessible :drink_type_id, :sugar_amount_id, :sugar_type_id, :milk_amount_id, :milk_type_id, :strength_id
 
   def description
-    "a coffee"
+    sentence = []
+    sentence << strength if strength.present?
+    sentence << drink_type
+
+    if milk_amount.present?
+      if milk_amount != "none"
+        sentence << "with"
+        sentence << "a"
+        sentence << milk_amount
+        sentence << milk_type if milk_type.present?
+        sentence << "milk"
+      else
+        sentence << "no milk"
+      end
+    end
+
+    if sugar_amount.present?
+      if sugar_amount > 0
+        sentence << "with"
+        if sugar_amount % 1 > 0
+          sentence << sugar_amount
+        else
+          sentence << sugar_amount.to_i
+        end
+        sentence << sugar_type if sugar_type.present?
+        sentence << "sugar"
+      else
+        sentence << "no sugar"
+      end
+    end
+
+    sentence.join(" ")
   end
 
   [:drink_type, :sugar_type, :sugar_amount, :milk_type, :milk_amount, :strength, :variation].each do |attribute|
