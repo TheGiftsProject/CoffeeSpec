1dbe38c1d6ba5f18136c049ebbab21ba73b8bc5c
yon <Lighthawky@gmail.com>
2012-05-09 16:59:38 +0300

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoffeeSpec::Application.config.secret_token = 'c7ab007ea9fa7c5f6b3f743b4de059ba59bfc7d1cf5c4f80d8ec57e500507ef2825b3cf2ed45e887571554008ffed289df85f82de07d85b8e01fd2208054da73'
