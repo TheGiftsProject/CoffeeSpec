5a64f21f5a6195b576ab50123dfc0528bc86dba4
yon <Lighthawky@gmail.com>
2012-05-09 18:15:43 +0300

    drink infographic works

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -9,7 +9,7 @@ class Drink < ActiveRecord::Base
   belongs_to :variation
 
   attr_accessible :name, :note
-  attr_accessible :drink_type_id
+  attr_accessible :drink_type_id, :sugar_amount_id, :sugar_type_id, :milk_amount_id, :milk_type_id, :strength_id
 
   def description
     "a coffee"
@@ -20,7 +20,9 @@ class Drink < ActiveRecord::Base
     alias_method old_attribute, attribute
     define_method(attribute) do
       object = send(old_attribute)
-      ActiveSupport::StringInquirer.new(object.value)
+      return ActiveSupport::StringInquirer.new if object.nil?
+      ActiveSupport::StringInquirer.new(object.value) if object.value.is_a?(String)
+      object.value
     end
   end
 
